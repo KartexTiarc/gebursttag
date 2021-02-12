62835c7349ee9cf18713a03e6c3b21738fffd5c6
KartexTiarc <christoffer.nyh@hotmail.com>
2021-02-12 16:44:41 +0000

    mera leka med javascript

--- assets/js/testing.js
+++ assets/js/testing.js
@@ -36,8 +36,26 @@ myFunc(function(value){
     console.log(value);
 });*/
 
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+people.forEach(function(person){
+    //create htlm template
+    html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html;
+
+/*const logPerson = (person, index) => {
+    console.log(`${index} - heelo ${person}`)
+}
+
+people.forEach(logPerson);
 
 people.forEach(function(person, index){
     console.log(index, person);
-});
+});*/
